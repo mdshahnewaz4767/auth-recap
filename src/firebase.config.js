bda6b0cabb910915243939ff29587dc73ed071ac
shahnewaz601 <shahnewaz601@gmail.com>
2021-03-15 20:43:41 +0600

    Implement Simple Google authentication system

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA3vwB2tKlMb5hGiTqrGBTDaVxGXUNo-ng",
+    authDomain: "auth-recap-b23f3.firebaseapp.com",
+    projectId: "auth-recap-b23f3",
+    storageBucket: "auth-recap-b23f3.appspot.com",
+    messagingSenderId: "242955436066",
+    appId: "1:242955436066:web:13dd699d6c4f92208cca7b"
+};
+
+export default firebaseConfig;
